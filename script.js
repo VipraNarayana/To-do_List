b19920183c9faa07efd68d61c9b50f9b59b2bb68
VipraNarayana <86181709+VipraNarayana@users.noreply.github.com>
2021-07-28 23:57:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,159 @@
+//Elements
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+// Classes
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle";
+const LINE_THROUGH = "lineThrough";
+
+// variables
+let LIST, id;
+
+// getItemFromLocalStorage
+
+let data = localStorage.getItem("TODO");
+
+
+if(data){
+    LIST = JSON.parse(data);
+    id = LIST.length;
+    loadList(LIST);
+}else{
+
+    LIST = [];
+    id = 0;
+}
+
+// loaoding Items to user-interface
+function loadList(array){
+    array.forEach(function(item){
+        addToDo(item.name, item.id, item.done, item.trash);
+    });
+}
+
+// clearing the localStorage
+clear.addEventListener("click", function(){
+    localStorage.clear();
+    location.reload();
+});
+
+
+// To-do function
+function addToDo(toDo, id, done, trash){
+
+    if(trash){ return; }
+
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done ? LINE_THROUGH : "";
+
+    const item = `<li class="item">
+                    <i class="far ${DONE} co" job="complete" id="${id}"></i>
+                    <p class="text ${LINE}">${toDo}</p>
+                    <i class="far fa-trash-alt de" job="delete" id="${id}"></i>
+                  </li>
+                `;
+
+    const position = "beforeend";
+
+    list.insertAdjacentHTML(position, item);
+}
+// add todo function
+function addTodoToTheList() {
+    const toDo = input.value;
+        if(toDo){
+            addToDo(toDo, id, false, false);
+            LIST.push({
+                name : toDo,
+                id : id,
+                done : false,
+                trash : false
+            });
+            localStorage.setItem("TODO", JSON.stringify(LIST));
+
+            id++;
+        }
+        input.value = "";
+
+}
+// item to list user the enter key
+document.addEventListener("keyup",function(even){
+    if(event.keyCode == 13){
+        addTodoToTheList();
+    }
+});
+
+
+// complete todo
+function completeToDo(element){
+    element.classList.toggle(CHECK);
+    element.classList.toggle(UNCHECK);
+    element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+
+    LIST[element.id].done = LIST[element.id].done ? false : true;
+}
+
+// remove todo
+function removeToDo(element){
+    element.parentNode.parentNode.removeChild(element.parentNode);
+
+    LIST[element.id].trash = true;
+}
+
+
+// targeting the elements created dynamically
+list.addEventListener("click", function(event){
+    const element = event.target;
+    const elementJob = element.attributes.job.value;
+
+    if(elementJob == "complete"){
+        completeToDo(element);
+    }else if(elementJob == "delete"){
+        removeToDo(element);
+    }
+
+    localStorage.setItem("TODO", JSON.stringify(LIST));
+});
+
+
+//Digital clock
+function updateClock(){
+  var now = new Date();
+  var dname = now.getDay(),
+      mo = now.getMonth(),
+      dnum = now.getDate(),
+      yr = now.getFullYear(),
+      hou = now.getHours(),
+      min = now.getMinutes(),
+      sec = now.getSeconds(),
+      pe = "AM";
+
+      if(hou >= 12){
+        pe = "PM";
+      }
+      if(hou == 0){
+        hou = 12;
+      }
+      if(hou > 12){
+        hou = hou - 12;
+      }
+
+      Number.prototype.pad = function(digits){
+        for(var n = this.toString(); n.length < digits; n = 0 + n);
+        return n;
+      }
+
+      var months = ["January", "February", "March", "April", "May", "June", "July", "Augest", "September", "October", "November", "December"];
+      var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+      var ids = ["dayname", "month", "daynum", "year", "hour", "minutes", "seconds", "period"];
+      var values = [week[dname], months[mo], dnum.pad(2), yr, hou.pad(2), min.pad(2), sec.pad(2), pe];
+      for(var i = 0; i < ids.length; i++)
+      document.getElementById(ids[i]).firstChild.nodeValue = values[i];
+}
+
+function initClock(){
+  updateClock();
+  window.setInterval("updateClock()", 1);
+}
